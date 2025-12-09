6dfa7211b4955121c41b082dd1ec2472a702a84d
YouSry3 <myosre122@gmail.com>
2025-12-09 17:24:31 +0200

    Feat: Build All End Point for 'ReportsController'

---
+++ BackEnd/FoodRescue.BLL/Contract/Reports/Create/ReportRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodRescue.BLL.Contract.Reports.Create
+{
+    public class ReportRequest
+    {
+        public Guid UserId { get; set; }
+        public Guid VendorId { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
