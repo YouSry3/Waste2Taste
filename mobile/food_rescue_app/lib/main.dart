6f9235d01222431db48143adc29f34cb7706383f
AbdelazizSameh <abdelzizsameh364@gmail.com>
2026-01-31 22:32:28 +0200

    add: initial mobile app

---
+++ mobile/food_rescue_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FoodRescueApp());
+}
+
+class FoodRescueApp extends StatelessWidget {
+  const FoodRescueApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: null);
+  }
+}
+
